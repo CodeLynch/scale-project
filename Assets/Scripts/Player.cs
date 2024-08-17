8a083d04dce7bdb90aa3a7e4d284aa81e54ce751
Damian <jei.grave@gmail.com>
2024-08-17 19:41:23 +0800

    Player and Camera Scripts
    Codes for both Camera and Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float AccelerationDash = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Movement
+        if(Input.GetKey("w")){
+            Debug.Log("W pressed");
+            transform.position = new Vector2(transform.position.x, transform.position.y + (speed * Time.deltaTime));
+        }
+        if(Input.GetKey("s")){
+            transform.position = new Vector2(transform.position.x, transform.position.y - (speed * Time.deltaTime));
+        }
+        if(Input.GetKey("a")){
+            transform.position = new Vector2(transform.position.x - (speed * Time.deltaTime), transform.position.y);
+        }
+        if(Input.GetKey("d")){
+            transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime), transform.position.y);
+        }
+        
+    }
+}
